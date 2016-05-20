a1c060a48ff970a517e07fc9598446ae0578e60a
Robbe De Geyndt <robbe.degeyndt@gmail.com>
2016-05-20 23:43:56 +0200

    Initial - Database connection, temporarily with Amazon AWS. - Adding students & courses. - Updating students & courses. - Error catching, with feedback for the user.

---
+++ labo04/Code/ISP-app with MongoDB/Edit.cs
@@ -0,0 +1,118 @@
+﻿using Logic;
+using Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISP_app_with_MongoDB
+{
+    public partial class Edit : Form
+    {
+        private Manipulations Logic { get; set; }
+        private Student Student { get; set; }
+        private Course Course { get; set; }
+        private string ObjectName { get; set; }
+
+        #region Constructors
+
+        public Edit(Manipulations logic, string objectName)
+        {
+            InitializeComponent();
+            tId.ReadOnly = false;
+            bDelete.Visible = false;
+            this.Logic = logic;
+            this.ObjectName = objectName;
+
+        }
+
+        public Edit(Manipulations logic, Student student)
+        {
+            InitializeComponent();
+            this.Logic = logic;
+            this.Student = student;
+            tId.Text = Student.Id.ToString();
+            tName.Text = Student.Name;
+            tYear.Text = Student.Year;
+        }
+
+        public Edit(Manipulations logic, Course course)
+        {
+            InitializeComponent();
+            this.Logic = logic;
+            this.Course = course;
+            tId.Text = Course.Id.ToString();
+            tName.Text = Course.Name;
+            tYear.Text = Course.Year;
+        }
+
+
+        #endregion
+
+        private void bDone_Click(object sender, EventArgs e)
+        {
+            if (Student != null)
+            {
+                try
+                {
+                    Student = new Student(Int32.Parse(tId.Text), tName.Text, tYear.Text);
+                    Logic.UpdateStudent(Student);
+                }
+                catch
+                {
+                    ErrorMessage("Something went wrong.\nPlease try again.");
+                }
+                this.Close();
+            }
+            else if (Course != null)
+            {
+                try
+                {
+                    Course = new Course(Int32.Parse(tId.Text), tName.Text, tYear.Text);
+                    Logic.UpdateCourse(Course);
+                }
+                catch
+                {
+                    ErrorMessage("Something went wrong.\nPlease try again.");
+                }
+                this.Close();
+            }
+            else if (ObjectName.Equals("Student"))
+            {
+                try
+                {
+                    Student = new Student(Int32.Parse(tId.Text), tName.Text, tYear.Text);
+                    Logic.AddStudent(Student);
+                }
+                catch
+                {
+                    ErrorMessage("Something went wrong.\nPossibly id [" + Student.Id + "] is already used.\nPlease try again.");
+                }
+                this.Close();
+            }
+            else if (ObjectName.Equals("Course"))
+            {
+                try
+                {
+                    Course = new Course(Int32.Parse(tId.Text), tName.Text, tYear.Text);
+                    Logic.AddCourse(Course);
+                }
+                catch
+                {
+                    ErrorMessage("Something went wrong.\nPossibly id [" + Course.Id + "] is already used.\nPlease try again.");
+                }
+                this.Close();
+            }
+        }
+
+        private void ErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ labo04/Code/ISP-app with MongoDB/Logic/Manipulations.cs
@@ -0,0 +1,105 @@
+﻿using Backend;
+using MongoDB.Driver;
+using Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Manipulations
+    {
+        public Connection MongoDBConnection { get; set; }
+
+        public Manipulations()
+        {
+            MongoDBConnection = new Connection("mongodb://student:Azerty123@ds011382.mlab.com:11382/csharpdblab04");
+        }
+
+        public void AddStudent(Student student)
+        {
+            MongoDBConnection.Students.InsertOne(student);
+        }
+
+        public void AddCourse(Course course)
+        {
+            MongoDBConnection.Courses.InsertOne(course);
+        }
+
+        public Student GetStudentOnName(string name)
+        {
+            List<Student> studenten = new List<Student>();
+
+            var filteredStudenten = from student in MongoDBConnection.Students.AsQueryable()
+                                    where student.Name.ToLower().Contains(name.ToLower())
+                                    select student;
+
+            foreach (Student x in filteredStudenten) studenten.Add(x);
+
+            if (studenten.Count != 1) throw new Exception("Error: Incorrect amount of results.");
+
+            return studenten.First();
+        }
+
+        public Student GetStudentOnID(int id)
+        {
+            List<Student> studenten = new List<Student>();
+
+            var filteredStudenten = from student in MongoDBConnection.Students.AsQueryable()
+                                    where student.Id.Equals(id)
+                                    select student;
+
+            foreach (Student x in filteredStudenten) studenten.Add(x);
+
+            if (studenten.Count != 1) throw new Exception("Error: Incorrect amount of results.");
+
+            return studenten.First();
+        }
+
+        public Course GetCourseOnName(string name)
+        {
+            List<Course> courses = new List<Course>();
+
+            var filteredCourses = from course in MongoDBConnection.Courses.AsQueryable()
+                                  where course.Name.ToLower().Contains(name.ToLower())
+                                  select course;
+
+            foreach (Course x in filteredCourses) courses.Add(x);
+
+            if (courses.Count != 1) throw new Exception("Error: Incorrect amount of results.");
+
+            return courses.First();
+        }
+
+        public Course GetCourseOnID(int id)
+        {
+            List<Course> courses = new List<Course>();
+
+            var filteredCourses = from course in MongoDBConnection.Courses.AsQueryable()
+                                  where course.Id.Equals(id)
+                                  select course;
+
+            foreach (Course x in filteredCourses) courses.Add(x);
+
+            if (courses.Count != 1) throw new Exception("Error: Incorrect amount of results.");
+
+            return courses.First();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            var filter = Builders<Student>.Filter.Eq("Id", student.Id);
+            var update = Builders<Student>.Update.Set("Name", student.Name).Set("Year", student.Year);
+            MongoDBConnection.Students.UpdateOne(filter, update);
+        }
+
+        public void UpdateCourse(Course course)
+        {
+            var filter = Builders<Course>.Filter.Eq("Id", course.Id);
+            var update = Builders<Course>.Update.Set("Name", course.Name).Set("Year", course.Year);
+            MongoDBConnection.Courses.UpdateOne(filter, update);
+        }
+    }
+}
